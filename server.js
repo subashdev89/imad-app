72158b335844e354315d652448ed04d0f065c78f
subashdev89 <31314579+subashdev89@users.noreply.github.com>
2017-09-26 13:07:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('article-one',function(req,res){
+        res.send('article one requested and will be served');
+});
+    
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
