8e2c4231c4e5a266b40c85e4c3229a54ea54f5cf
subashdev89 <31314579+subashdev89@users.noreply.github.com>
2017-09-28 16:24:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,1 @@
 console.log('Loaded!');
-var img=document.getElementById('madi');
-var marginleft=0;
-function moveright(){
-    marginleft=marginleft+10;
-    img.style.marginleft=marginleft+'px';
-    }
-    img.onclick=function(){
-        var interval=setInterval(moveright, 50);
-    };
